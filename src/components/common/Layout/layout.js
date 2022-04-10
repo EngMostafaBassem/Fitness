af5e5339fe903fd04f8bebda160794a32f63ce83
vip <vip@DESKTOP-TVLU5IH>
2022-04-10 19:45:26 +0200

    update layout for footer

--- src/components/common/Layout/layout.js
+++ src/components/common/Layout/layout.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Footer from '../Footer'
 import Navbar from '../Nav'
+import styles from './index.module.scss'
 const Layout=({children})=>{
   return(
-      <>
+    <>
+      <div className={styles['container']}>
         <Navbar/> 
-        {children}
-        <Footer/>
+         {children} 
+      </div>
+      <Footer/>
       </>
   )
 }
